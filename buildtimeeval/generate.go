e4fd1d670afb0dbab5893e17925942475a0d223b
Dan Chao <dan.chao@apple.com>
2025-11-17 13:45:10 -0800

    Migrate to GitHub Actions
    Also:
    
    * Bump Go to 1.25
    * Bump Pkl to 0.30
    * Add hawkeye license formatting
    * Add checks for hawkeye formatting and pkl formatting

--- buildtimeeval/generate.go
+++ buildtimeeval/generate.go
@@ -1,18 +1,18 @@
-// ===----------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 // Copyright © 2025 Apple Inc. and the Pkl project authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	https://www.apache.org/licenses/LICENSE-2.0
+//   https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// ===----------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 //go:generate go tool pkl-gen-go pkl/AppConfig.pkl
 //go:generate sh -c "pkl eval -f pkl-binary pkl/*/*.pkl -o configdata/%{moduleDir}/%{moduleName}.msgpack"
